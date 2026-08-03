1a1c1227f30dd9b5908d73978b84ea74fa4db22c
pandity2007-code <pandity2007@gmail.com>
2026-08-03 23:49:42 +0530

    Type function

---
+++ chapter_2/04_type.py
@@ -0,0 +1,26 @@
+a = 35 #<class 'int'>
+t= type (a) #type function 
+
+print(t)
+b= 2.34  #<class 'float'>
+ty=type (b)
+print(ty) 
+
+c = "yasshu"  #<class 'str'>
+typ= type (c)
+print(typ) 
+
+
+# changing one data type into another
+
+a = "45.5"   #<class 'str'>
+b =type(a)
+print(b)
+
+#  changing str datatype into floating 
+
+a = "45.5"   #<class 'str'>
+b = float(a)
+t = type(b)
+print(t)
+ 
