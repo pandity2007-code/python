580af3d037d3806d8d99f535bf263cafaa965d5f
pandity2007-code <pandity2007@gmail.com>
2026-08-04 00:34:20 +0530

    input function

---
+++ chapter_2/05_input.py
@@ -0,0 +1,24 @@
+a = input("enter the number1 : \n")
+
+
+b = input("enter the number2 : \n")
+
+
+print("number1 is = ",a)
+print("number2 is = ",b)
+
+print("'sum is :",a+b) # because the value of a & b are string type 
+
+# for the correct sum of value
+
+# here we use int for changing the input value str to int
+a = int(input("enter the number1 : \n"))
+
+
+b = int (input("enter the number2 : \n"))
+
+
+print("number1 is = ",a)
+print("number2 is = ",b)
+
+print("'sum is :",a+b) 
