68b0a2439f7825a4304174c7477c98ef39c264c8
pandity2007-code <pandity2007@gmail.com>
2026-08-05 01:34:24 +0530

    string Functions

---
+++ chapter_3/04_string_function.py
@@ -0,0 +1,49 @@
+# 01 len()function
+
+name = "yasshu"
+
+print(len(name))   #returns the length of string 
+
+
+# 02 string.endswith("") function
+
+mystring = "yash"
+
+print(mystring.endswith("sh"))  #check the string is end with given ("sh") or not
+
+#03 sring.startswith("")function.
+
+mystring = "yash"
+
+print(mystring.startswith("ya")) # check the string is start with given ("y") or not
+
+#04 string.capitalized() Function
+
+a = "harsha"
+
+print(a.capitalize()) #capitalizes the first character of given string
+
+
+#05 string.find("word")   
+
+b = "hello world"
+
+print(b.find(" world"))  #this function find the word and return the index of first occurances of that word
+
+
+#06 string.count("c") function
+
+
+s = "saraswat"
+
+print(s.count("c"))
+
+
+
+#07 string.replace("old world" , "new world") 
+
+
+z = "hello world"    #replace the old word with new  word
+
+
+print(z.replace("world", "python"))
