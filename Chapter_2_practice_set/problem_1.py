72d16cd9eefe504db13bc5abc8e14272285acd1a
pandity2007-code <pandity2007@gmail.com>
2026-08-04 18:08:47 +0530

    add of two numbers

---
+++ Chapter_2_practice_set/problem_1.py
@@ -0,0 +1,15 @@
+ # sum of two numbers
+
+a = 3
+
+b = 5
+
+print(a+b)
+
+# taking input from the user and sum 
+
+a = int(input("number a is :"))
+b = int(input("number b is :"))
+
+c = a+b 
+print("sum is :",c)
