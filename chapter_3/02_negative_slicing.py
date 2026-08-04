a63ab20e8e116c882903504cde0f5e8701741697
pandity2007-code <pandity2007@gmail.com>
2026-08-05 00:34:38 +0530

    Negative slicing

---
+++ chapter_3/02_negative_slicing.py
@@ -0,0 +1,12 @@
+#Negative slicing
+
+name = "yasshu"
+
+# print(name[0:3]) # positive slicing
+
+
+print(name[-5:-2] )   # negative slicing
+
+print(name[0: ])    # is same as name[0:length-1]
+print(name[:5])    # is same as name[0:5]
+
