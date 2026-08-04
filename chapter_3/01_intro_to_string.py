048be0c36f748904b624f397e0901c943acf1834
pandity2007-code <pandity2007@gmail.com>
2026-08-05 00:00:47 +0530

    string

---
+++ chapter_3/01_intro_to_string.py
@@ -0,0 +1,13 @@
+#introductio of string
+
+#length of string
+name = "yasshu"   # string
+
+length = len(name)
+print(length )
+
+#shortstring
+
+name1 = "harry"
+short = name1[0:3] #start from index 0 all the way till 3 (excluding 3)
+print(short)
