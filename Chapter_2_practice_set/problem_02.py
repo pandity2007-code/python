c86d245bb24d1444ca7875d35a70413611e10e43
pandity2007-code <pandity2007@gmail.com>
2026-08-04 18:20:16 +0530

    finding remainder of  numbers

---
+++ Chapter_2_practice_set/problem_02.py
@@ -0,0 +1,8 @@
+# find the remainder when a number divided by a number
+
+a = int(input("enter the number a :"))
+
+b = int(input("enter the number b :"))
+
+c = a%b     # mudule operator gives the remainder
+print(" the remainder of number a when divided by b:",c)
