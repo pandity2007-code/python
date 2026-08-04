1961476be0db0f6867c28c7e3653792502119084
pandity2007-code <pandity2007@gmail.com>
2026-08-04 18:56:42 +0530

    comparison operator

---
+++ Chapter_2_practice_set/problem_04.py
@@ -0,0 +1,7 @@
+#comparison operator.
+# checking the a is greather than b or not.
+a = int(input("enter the number a: "))
+b = int(input("enter the number b: "))
+
+
+print("a is greater gthan or not is ", a>b)
