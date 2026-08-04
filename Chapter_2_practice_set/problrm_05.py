c01c0cf14bb5d73524323fb28105ba3320820fe4
pandity2007-code <pandity2007@gmail.com>
2026-08-04 19:06:30 +0530

    Average of Two numbers

---
+++ Chapter_2_practice_set/problrm_05.py
@@ -0,0 +1,10 @@
+# Finding the average of two numbers.
+
+a = int(input("enter the number a :"))
+
+
+b = int(input("enter the number b :"))
+
+c = (a+b)/2 #formula of avg
+
+print("average of two number is:",c)
