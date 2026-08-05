b1391e4aab8541de1a0bb24bae07c7ad79b1d4df
pandity2007-code <pandity2007@gmail.com>
2026-08-05 23:54:27 +0530

    using find function

---
+++ chapter_3_practice_set/problrm_03.py
@@ -0,0 +1,10 @@
+# find double space in the string
+
+name = "my name is yash saraswat  and iam from aligarh"
+
+#using string.find("") function
+
+print(name.find("  "))
+
+# if output comes -1 then there is not  word that we trying to find 
+# if output comes -1< then output gives us the index of that word
