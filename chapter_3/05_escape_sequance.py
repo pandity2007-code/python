3da25ccd4562c63d8448a436adeb22c828eba91d
pandity2007-code <pandity2007@gmail.com>
2026-08-05 23:05:18 +0530

    escape sequance

---
+++ chapter_3/05_escape_sequance.py
@@ -0,0 +1,23 @@
+
+# \n - for next line
+
+a = "hello i am yash \n i am a cse student"
+
+print(a)
+
+# \t - for tab size space
+
+a = "hello i am yash \t i am a cse student"
+
+print(a)
+
+# \"" - for double quates in string
+
+b = "hello i am yash \" i am a cse student\""
+
+print(b)
+
+# \\ - for baclslash
+
+a = "hello i am yash \\ i am a cse student"
+print(a)
