01b76c7017c3f664808db46cbf86951d92ae4888
pandity2007-code <pandity2007@gmail.com>
2026-08-07 00:39:26 +0530

    Tuple methods

---
+++ chapter_4/09_tuple_methods.py
@@ -0,0 +1,16 @@
+# Method of tuples
+
+# tuple.count()
+
+a = (1,4,2,6,8)
+
+b = a.count(2)  # returns number of times a specified value appears in tuple
+
+print(b)
+
+# tuple.index()
+
+x = (4,33,22,6,7,8.0)
+
+y = x.index(4) # will return first occurance of () value in x 
+print(y)
