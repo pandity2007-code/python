80085b774a914233e6ca88d01e813c9d453fea23
pandity2007-code <pandity2007@gmail.com>
2026-08-07 00:52:13 +0530

    some other methods of  tuple

---
+++ chapter_4/10_additional_methods_tuple.py
@@ -0,0 +1,16 @@
+#concatenation 
+tuple1 = (1,2,3)
+tuple2 = (4,5,6)
+concatenation=tuple1 + tuple2
+print(concatenation)
+
+#repeatation 
+
+tuple = (1,2,3,4,5)
+repeatation = tuple*3
+print(repeatation)
+
+#length
+
+a = (1,2,3,4,5)
+print(len(a))
