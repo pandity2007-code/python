bee23bd5efdf3a1707601d8377e0b454a0cb4f6a
pandity2007-code <pandity2007@gmail.com>
2026-08-07 01:51:23 +0530

    print 5 stdent marks and display them in sorted manner

---
+++ chapter_4_practice_set/problrm_02.py
@@ -0,0 +1,21 @@
+# problem 2
+
+marks = []
+
+
+student1 = (input("marks of 1st student1 :"))
+marks.append(student1)
+student2 = (input("marks of st student2 :"))
+marks.append(student2)
+student3 = (input("marks of st student3:"))
+marks.append(student3)
+student4 = (input("marks of st student4:"))
+marks.append(student4)
+student5 = (input("marks of st student5 :"))
+marks.append(student5)
+
+
+
+
+marks.sort()
+print(marks)
