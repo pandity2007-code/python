2e4b4e6b09328ac5dfcd0d713c68a529410d767e
pandity2007-code <pandity2007@gmail.com>
2026-08-06 23:55:38 +0530

    pop method

---
+++ chapter_4/06_pop method.py
@@ -0,0 +1,9 @@
+# pop method
+list = [2,4,3,7,9,5,4]
+list.pop(2)  #will delete element at index 2.
+
+print(list)
+
+# if we want to print list.pop(2) the output will comes of index 2 element 
+
+print(list.pop(2) )
