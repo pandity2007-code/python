24f72c9926a70007a7ffd796e6fb920cfc7150af
pandity2007-code <pandity2007@gmail.com>
2026-08-09 00:14:33 +0530

    update method

---
+++ chapter_5/05_dict_update_method.py
@@ -0,0 +1,11 @@
+#update method
+
+
+marks = {
+    "yash" : 100,
+    "nakul":56,
+    "aman":44
+}
+marks.update({"aman":00}) #update method change the values of dictionary keys because dictionary is mutable
+
+print(marks)
