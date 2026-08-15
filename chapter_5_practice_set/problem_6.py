67e1cc7d431af7adb875edf3643a3f29a40a11be
pandity2007-code <pandity2007@gmail.com>
2026-08-15 20:15:26 +0530

    problem 6

---
+++ chapter_5_practice_set/problem_6.py
@@ -0,0 +1,28 @@
+#using empty dictionary allow 4 friends to enter their name and fvrt language as value and key
+
+
+
+d = {}
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+
+print(d)
