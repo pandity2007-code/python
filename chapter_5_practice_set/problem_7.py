446449069ead4fe3d8ab9827d9ca23f875901f53
pandity2007-code <pandity2007@gmail.com>
2026-08-15 20:22:17 +0530

    if two frnds have same name in problem 6

---
+++ chapter_5_practice_set/problem_7.py
@@ -0,0 +1,27 @@
+# if two frirnds have same name then
+
+d = {}
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+name = input("enter thr friend name :")
+lang = input("enter the fvrt language name:")
+d.update({name:lang})
+
+
+print(d)
+# if two freinds have name nakul :cpp or nakul:js so if we enter first js as fvrt lang of nakul then enter second nakul fvrt lang cpp so the output is cpp
