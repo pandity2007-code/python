37d4766899ae2ba16170fc5bd7a02a622128a8fb
pandity2007-code <pandity2007@gmail.com>
2026-08-17 15:52:09 +0530

    checking the giving name is in the list or not

---
+++ chapter_6_practice_set/problem_05.py
@@ -0,0 +1,10 @@
+# checking the giving name is in the list or not
+
+list = ["yasshu","nakul","dhruv","alok","chaubey"]
+
+name = input("enter the name:")
+
+if(name in list):
+    print("yess name is in the list")
+else:
+    print ("the name is not in the list")    
