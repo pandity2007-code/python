5d74238607a3787edd7a542057507028991ab4ca
pandity2007-code <pandity2007@gmail.com>
2026-08-17 22:53:46 +0530

    calculate the grade of student from his marks

---
+++ chapter_6_practice_set/problem_06.py
@@ -0,0 +1,18 @@
+marks = int(input("enter your marks:"))
+
+if (100>=marks>90):
+    print("you are passed with grade : ex")
+
+elif(90>=marks>=80):    
+    print("you are passed with grade : A")
+elif(80>marks>=70):
+    print("you are passed with grade : B")
+elif(70>marks>=60):
+    print("you are passed with grade : C")
+
+elif(60>marks>=50):
+    print("you are passed with grade :D")
+
+else:
+    print("you are failed")        
+        
