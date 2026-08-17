33ae0922508ac3285a29cdab183c56e4f35bbfcb
pandity2007-code <pandity2007@gmail.com>
2026-08-17 23:05:44 +0530

    problem 7

--- chapter_6_practice_set/problem_07.py
+++ chapter_6_practice_set/problem_07.py
@@ -1,7 +1,7 @@
 post = input("enter your post:")
 
 
-if("yash" in post):
+if("yash".lower() in post.lower()):
     print("the post is talking about the yash")
 else:
     print("the post is not talking about the yash")
