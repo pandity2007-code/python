6ba900c1c177582172233249ea5ac35845cc7742
pandity2007-code <pandity2007@gmail.com>
2026-08-18 12:58:25 +0530

    for loops iteration (lists,tuple,string)

---
+++ chapter_7/06_for_loops_iteration.py
@@ -0,0 +1,21 @@
+# for loop iteration with lists
+
+l =[2,3,5,7,88,6554,3,25,"yasshu"]
+
+for i in l:
+    print(i)
+
+
+#for loop iteration with tuples
+
+t=(34,45,66,233,"yash")
+for i in t:
+    print(i)
+
+
+# for loop with string
+
+s = "pandit"
+
+for i in s:
+    print(i)
