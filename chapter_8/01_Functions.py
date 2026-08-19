5cbbffe04f199aa959d2f87c112117485bae56fa
pandity2007-code <pandity2007@gmail.com>
2026-08-19 16:31:46 +0530

    functions

---
+++ chapter_8/01_Functions.py
@@ -0,0 +1,15 @@
+# printing average using functions
+
+def avg():       # function defination
+
+    a = int(input("enter the number 1st:"))
+
+    b = int(input("enter the number 2nd:"))
+
+    c = int(input("enter the number 3rd:"))
+
+    average=(a+b+c)/3
+    print(average)
+
+avg()   #function call
+avg()
