80fcba796ef2a30fed0bd60a005e54166e6486dd
pandity2007-code <pandity2007@gmail.com>
2026-08-22 20:02:44 +0530

    celcius to fahrenheit

--- chapter_8_practice_set/problem_2.py
+++ chapter_8_practice_set/problem_2.py
@@ -2,10 +2,10 @@
 
 # formula = c/5 = f-32/9
 #c= 5*(f-32/9)
-#f= (c/5)*5-32
+#f= (c*9/5)-32
 
 c = int(input("enter the temp in celcius:"))
 
 def temp(c):
-    return (c/5)*5-32
+    return (c*9/5)+32
 print(f"celcius in fahrenhiet : {temp(c)}")
