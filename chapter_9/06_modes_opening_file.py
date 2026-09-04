59a489d72e559c4bfb39efccd67e8ad2572a2aa2
pandity2007-code <pandity2007@gmail.com>
2026-09-04 23:14:53 +0530

    modes of opening file

---
+++ chapter_9/06_modes_opening_file.py
@@ -0,0 +1,13 @@
+#modes of opening a file in python
+
+# "r" open for reading a file 
+
+#"w" open for writing in a file
+
+#"a" open for insertion in a file
+
+#"+"open  for updating in a file
+
+#"rb"open  for read  in binary mode
+
+#"rt"open  for read  in text mode
