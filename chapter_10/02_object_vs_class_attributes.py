50cf516f9d3ef7013e45c9a805e178d6dedbc817
pandity2007-code <pandity2007@gmail.com>
2026-09-05 23:51:03 +0530

    attributes

---
+++ chapter_10/02_object_vs_class_attributes.py
@@ -0,0 +1,13 @@
+class Employee:
+    
+    age=18
+    language="py"
+
+harry=Employee()
+harry.language="c"
+
+print(harry.language, harry.age)
+
+
+# object attributes take precedence over the class attributes
+
