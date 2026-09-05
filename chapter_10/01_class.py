2e0fe70fe7edfa8f688b6dd6c0246beff5b0e2a4
pandity2007-code <pandity2007@gmail.com>
2026-09-05 23:43:04 +0530

    class ,objects and attributes

---
+++ chapter_10/01_class.py
@@ -0,0 +1,15 @@
+class Employee:
+    
+    age=18
+    language="py"
+
+harry=Employee()
+harry.name="yash"
+print(harry.name,harry.language, harry.age)
+
+
+rohan=Employee()
+rohan.name="rohan"
+print(rohan.name,rohan.age,rohan.language)
+
+# here name is object attributes and age or language are class attributes as they directly belongs to class
