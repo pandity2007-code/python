6c10693f2d0769182d4c1f5995450191f970c771
pandity2007-code <pandity2007@gmail.com>
2026-09-05 15:10:54 +0530

    problem 7

---
+++ chapter_9_practice_set/problem_7.py
@@ -0,0 +1,11 @@
+with open("log.txt","r") as f:
+    lines=f.readlines()
+
+lineno=1    
+for line in lines:
+    if "python " in line:
+        print(f"yes word is present in this file lineno:{lineno}")
+        break
+    lineno+=1
+else:
+    print("no this word is not present in file")
