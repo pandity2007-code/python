ddc18758fdddf2a33c294b83d51c3d912c670378
pandity2007-code <pandity2007@gmail.com>
2026-09-06 19:52:50 +0530

    problem 3

---
+++ chapter_10_practice_set/problem_3.py
@@ -0,0 +1,9 @@
+class x:
+    a=4
+o=x()
+print(o.a)
+o.a=0
+print(o.a)
+
+# if we make a class attribute a and give some value than we make object attribute object.a=0 does it change class Attribute 
+# answer is no 
