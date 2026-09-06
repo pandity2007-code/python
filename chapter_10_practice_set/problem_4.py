e48ba51259b23292a4f961518276906c4651fffe
pandity2007-code <pandity2007@gmail.com>
2026-09-06 23:00:02 +0530

    using @staticmethod

---
+++ chapter_10_practice_set/problem_4.py
@@ -0,0 +1,21 @@
+class calculator:
+    def __init__(self,n):
+        self.n=n
+    
+    
+    
+    def square(self):
+        print(f"square of {self.n} : {self.n*self.n}") 
+    def cube(self):
+        print(f"cube of {self.n}:{self.n**3}")       
+    def cuberoot(self):
+        print(f"cube of {self.n}:{self.n**1/2}")     
+    @staticmethod
+    def hello():
+        print("hellooo!")      
+
+a=calculator(4) 
+a.square()       
+a.cube()
+a.cuberoot()
+a.hello()
