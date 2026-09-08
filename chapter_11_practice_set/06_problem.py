8f78161da4f0a5f9c96395e51800e4247e3a2cbc
pandity2007-code <pandity2007@gmail.com>
2026-09-08 12:21:02 +0530

    add & multiple of vector using operator overloading

---
+++ chapter_11_practice_set/06_problem.py
@@ -0,0 +1,19 @@
+class vector:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
+    def __add__(self, other):
+        result=vector(self.x+other.x,self.y+other.y,self.z+other.z)   
+        return result 
+    def __mul__(self, other):
+        result= self.x*other.x + self.y*other.y + self.z*other.z
+        return result
+    def __str__(self):
+        return f"{self.x},{self.y},{self.z}"
+
+v1=vector(1,2,3)
+v2=vector(4,1,3)
+v3=vector(1,5,1)
+print(v1+v2+v3)
+print(v1*v2)
