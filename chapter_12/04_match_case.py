375ae1bc33fcc60beefd5246f9441586dea4e700
pandity2007-code <pandity2007@gmail.com>
2026-09-09 15:13:57 +0530

    match case

---
+++ chapter_12/04_match_case.py
@@ -0,0 +1,9 @@
+def check_day(day):
+    match day:
+        case 1:
+            return "monday"
+        case 2:
+            return "tuesday"
+        case 3:
+            return "wednesday"
+print(check_day(1))        
