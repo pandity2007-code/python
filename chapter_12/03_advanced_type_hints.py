f8d6ed1917206004ae5e079161bdfd0763a2d082
pandity2007-code <pandity2007@gmail.com>
2026-09-09 11:05:53 +0530

    advanced type hint

---
+++ chapter_12/03_advanced_type_hints.py
@@ -0,0 +1,12 @@
+from typing import List,tuple,Dict,Union
+
+n:list[int]=[1,2,3,45,5]
+
+a:tuple[int,str]=(23,"yash")
+
+b:dict[str, int | str]={
+    'n': 33,
+    "name":"yash",
+    "age": 18,
+
+}
