edea1967fcdbdf1f50b1d95eb93e9831f81321d2
pandity2007-code <pandity2007@gmail.com>
2026-09-10 12:17:31 +0530

    enumerate function

---
+++ chapter_12/14_enumerate.py
@@ -0,0 +1,10 @@
+l=[1,2,3,4,5]
+
+# index=0
+
+# for item in l:
+#     print(f"the item number at {index}is{item}")
+#     index+=1
+
+for index,item in enumerate(l):   # this is done by enumerate function
+    print(f"the item number at {index}is{item}")
