d6bc7009cfbf6d8d314b613648eb48484c849d36
pandity2007-code <pandity2007@gmail.com>
2026-09-11 12:28:50 +0530

    virtual enviroments

---
+++ chapter_13/01_venv.py
@@ -0,0 +1,21 @@
+#making the virtual enviroment
+
+# hoe to install virtual enviroment
+
+#pip install virtualenv
+# virtualenv env (here env is the name of file )
+
+# then we install some package ex
+#pip install pandas
+# then 
+#pip install pandas==1.5.1 (another version of pandas)
+#if we wanted to use version or activate i write ./env/scripts/activate.ps
+#if i want to check which which version i was installed 
+# use pip freeze
+
+#if we want to write these version in txt file we can write using
+
+# pip freeze >requirements.txt
+
+#if we want to share these version or want to recreate 
+# pip install -r./requirements.txt se sare cheeze install ho jayegi
