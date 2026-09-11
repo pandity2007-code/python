0fe19079dff1fed917e2ca7010ef84ac2522656f
pandity2007-code <pandity2007@gmail.com>
2026-09-11 23:49:52 +0530

    using format function

---
+++ chapter_13_practice_set/01_problem.py
@@ -0,0 +1,7 @@
+name = input("enter the name of student:")
+
+marks= int(input("marks of student:"))
+
+phone= int(input("phone no:"))
+
+a="The name of student is {name} and his  marks are {marks} and phone number is{phone} ".format(name,marks ,phone)
