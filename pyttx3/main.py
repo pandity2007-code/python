723e44fc270e3415fad65329f892ee3ad68335e1
pandity2007-code <pandity2007@gmail.com>
2026-09-12 11:14:04 +0530

    using pyttsx3

---
+++ pyttx3/main.py
@@ -0,0 +1,7 @@
+import pyttsx3 as pt
+
+engine=pt.init()   # use for making object
+
+engine.say("hey yash , how can i help you")   #say method
+
+engine.runAndWait()   #runANdwait( ) use for stop the program after say
