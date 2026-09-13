c961a07a6940a4ad9d70fb10d9588e9a08a24c7c
pandity2007-code <pandity2007@gmail.com>
2026-09-13 19:19:37 +0530

    add,subs,mult,division of two number

---
+++ Revision/03_Q.py
@@ -0,0 +1,11 @@
+# Take two numbers and perform addition, subtraction, multiplication and division.
+
+
+
+a = 20
+b = 5
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
