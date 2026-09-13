692ca50ff16c33281c46189aa7535343e1ccff61
pandity2007-code <pandity2007@gmail.com>
2026-09-13 19:39:28 +0530

    percentage of student

---
+++ Revision/09_Q.py
@@ -0,0 +1,11 @@
+math = 80
+physics = 75
+chemistry = 85
+english = 70
+python = 90
+
+total = math + physics + chemistry + english + python
+percentage = total / 5
+
+print("Total Marks =", total)
+print("Percentage =", percentage)
