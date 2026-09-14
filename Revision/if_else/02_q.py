9f469d8daf04788a81ec1386fde4eb4014086d35
pandity2007-code <pandity2007@gmail.com>
2026-09-14 23:36:37 +0530

    Create a simple grading system.

---
+++ Revision/if_else/02_q.py
@@ -0,0 +1,13 @@
+# Create a simple grading system.
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+elif marks >= 60:
+    print("Grade D")
+else:
+    print("Fail")
