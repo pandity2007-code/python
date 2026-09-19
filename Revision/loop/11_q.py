f68a932e07501c4ac8def7ff5079873f4184bb3c
pandity2007-code <pandity2007@gmail.com>
2026-09-19 11:39:06 +0530

    greet all the person name stored in l  with hello which start with s

---
+++ Revision/loop/11_q.py
@@ -0,0 +1,7 @@
+# greet all the person name stored in l  with hello which start with s 
+
+l=["yash","dhruv","shyam","sivam","aarnav"]
+
+for i in l:
+    if(i.startswith("s")):
+        print(f"hello,{i}")
