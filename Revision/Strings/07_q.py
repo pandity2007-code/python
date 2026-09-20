539b8d65b7285e71730f5c0a8d7f39db7492f0ca
pandity2007-code <pandity2007@gmail.com>
2026-09-20 23:54:39 +0530

    using f string

---
+++ Revision/Strings/07_q.py
@@ -0,0 +1,8 @@
+# Using "f"string , create a sentence: "My name is John and I am 25 years old."
+#  by passing "John" and 25 as variables.
+
+name="john"
+age="25"
+
+sentence=f"My name is {name} and I am {age} years old."
+print(sentence)
