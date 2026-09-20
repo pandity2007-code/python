b3a801af1cf904afc35aedd5d34919a2f7833ac8
pandity2007-code <pandity2007@gmail.com>
2026-09-20 23:21:05 +0530

    string slicing and indexing

---
+++ Revision/Strings/03_q.py
@@ -0,0 +1,8 @@
+text="python programming"
+si1=text[0:6] #Print the first 6 characters
+
+print(si1)
+si2=text[7:] #  Print the last 6 characters
+print(si2)
+print(text[0:17:2])
+#   Print every second character from the string
