a1513b8e7aaed9e8228fc47d0ee5fe6af1d92941
pandity2007-code <pandity2007@gmail.com>
2026-09-20 23:48:33 +0530

    Using format()

---
+++ Revision/Strings/06_q.py
@@ -0,0 +1,8 @@
+# Using format() , create a sentence: "My name is John and I am 25 years old."
+#  by passing "John" and 25 as variables.
+
+name="john"
+age="25"
+
+sentence="My name is {} and I am {} years old.".format(name,age)
+print(sentence)
