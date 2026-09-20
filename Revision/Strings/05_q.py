5f5d9d925bce63dbbe38b38cc488c6658040fa24
pandity2007-code <pandity2007@gmail.com>
2026-09-20 23:41:43 +0530

    using string functions

---
+++ Revision/Strings/05_q.py
@@ -0,0 +1,10 @@
+text=" i love python programming "
+a=text.strip()   # Remove extra spaces from both ends
+print(a)
+
+b=text.title() #Convert it to title case
+print(b)
+
+c=text.count("o")  #count how many times o appears in string
+print(c)
+
