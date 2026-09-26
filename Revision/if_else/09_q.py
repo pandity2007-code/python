2c390f6abf374db13bfd3ead36b5fee6e8966b4f
pandity2007-code <pandity2007@gmail.com>
2026-09-26 18:47:25 +0530

    Find the greater of two numbers

---
+++ Revision/if_else/09_q.py
@@ -0,0 +1,10 @@
+# Find the greater of two numbers.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print(a, "is greater")
+elif b > a:
+    print(b, "is greater")
+else:
+    print("Both numbers are equal")
